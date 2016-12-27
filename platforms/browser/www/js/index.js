e877407d1c1956d77dec54058961631d0d72c601
Marco Carrizales <marco_carrizales@msn.com>
2016-12-26 23:06:24 -0600

    Changes in the interface
    The quotes now are aligned to the left.
    The text for the user on the terminal was changed.
    The time for the command's animation was reduced and its text was changed.
    The status bar color was changed.
    The background was rotated to portrait mode.
    The app were locked to be seen only in portrait mode.
    The name of the app was changed.

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -1,50 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    },
-
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-    },
-
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-    }
-};
-
-app.initialize();
-
 var user = "tamoxin@codepen:~$ ";
 
 var shareLink = "";
@@ -58,6 +11,7 @@ var quote;
 var author;
 
 var insertText = function(){
+  jQuery.support.cors = true;
   $.ajax({
       url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
       success: function(a) {
@@ -68,13 +22,28 @@ var insertText = function(){
         tweet += $("#quote").text()+ "\n" + $("#author").text();
         shareLink = encodeURI(tweet);
       },
+      error: function(jqXHR, textStatus, errorThrown) {
+        $(insertError);
+      },
       cache: false
    });
 
 }
 
+var insertError = function(){
+  quote = "<p style='text-align: left'>There was an error while retrieving your quote.<br>Please check your internet connection and try again.</p>";
+  $("#quote").html(quote);
+  tweet += $("#quote").text();
+  shareLink = encodeURI(tweet);
+}
+
 var insertAbout = function(){
-  quote = "<p style='text-align: left'>This random quote machine was made by <a href='http://marcocarrizales.me/' target='_blank' class='links'>Marco Carrizales</a>.<br>Thanks to <a href='https://twitter.com/atmattb' target='_blank' class='links'>Matt Boldt</a> for the <a href='http://www.mattboldt.com/demos/typed-js/' target='_blank' class='links'>Typed plugin</a>.<br>The quotes showed on this site are from <a href='https://quotesondesign.com/' target='_blank' class='links'>https://quotesondesign.com/</a></p>";
+  quote = "<p style='text-align: left'>This random quote machine was made by "+
+    "<a href='http://marcocarrizales.me/' target='_blank' class='links'>Marco Carrizales</a>."+
+    "<br>Thanks to <a href='https://twitter.com/atmattb' target='_blank' class='links'>Matt Boldt"+
+    "</a> for the <a href='http://www.mattboldt.com/demos/typed-js/' target='_blank' class='links'>"+
+    "Typed plugin</a>.<br>The quotes showed on this site are from <a href='https://quotesondesign.com/' "+
+    "target='_blank' class='links'>https://quotesondesign.com/</a></p>";
   author = "Thanks for passing by :)"
   $("#quote").html(quote);
   $("#author").html(author);
@@ -111,16 +80,54 @@ var clr = function(){
   $("#author").text(author);
 }
 
-$(document).ready(function(){
-  $(newQuote);
-  // new quote
-  $("#new-quote").on("click", newQuote);
+// cordova code beginning
+var app = {
+    // // Application Constructor
+    initialize: function() {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+        // initialize jQuery methods
+        $(document).ready(function(){
+            $(newQuote);
+            // new quote
+            $("#new-quote").on("click", newQuote);
 
-  //about
-  $("#about").on("click", about);
+            //about
+            $("#about").on("click", about);
 
-  //share quote
-  $("#share-quote").on("click", function(){
-    $("#share-quote").attr("href", shareLink);
-  });
-});
+            //share quote
+            $("#share-quote").on("click", function(){
+                $("#share-quote").attr("href", shareLink);
+            });
+        });
+    },
+
+    // deviceready Event Handler
+    //
+    // Bind any cordova events here. Common events are:
+    // 'pause', 'resume', etc.
+    onDeviceReady: function() {
+        //this.receivedEvent('deviceready');
+    },
+
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        
+    }
+};
+
+app.initialize();
+
+// $(document).ready(function(){
+//     $(newQuote);
+//     // new quote
+//     $("#new-quote").on("click", newQuote);
+
+//     //about
+//     $("#about").on("click", about);
+
+//     //share quote
+//     $("#share-quote").on("click", function(){
+//         $("#share-quote").attr("href", shareLink);
+//     });
+// });
+
